73e48e3a959d323ebf6ef6b2655fc711b1e4bd10
Tazeem Akhtar <tazeem.akhtar@mayabytes.com>
2024-06-03 22:06:11 +0500

    Header

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,17 +1,89 @@
-import { ModeToggle } from "@/components/mode-toggle"
+"use client"
 
-export default function Header({
-  children,
-}: Readonly<{ children?: React.ReactNode }>) {
-  return (
-    <header className="mx-auto flex w-full items-center justify-between px-4 py-8">
-      <a href="/" className="text-xl font-bold text-rose-400">
-        Mayabytes
-      </a>
-      <nav className="flex flex-wrap justify-center gap-2">{children}</nav>
-      <div>
-        <ModeToggle />
-      </div>
-    </header>
-  )
-}
+import { forwardRef, HTMLAttributes, RefObject, useRef } from "react"
+import Image from "next/image"
+import Link from "next/link"
+import { motion, useInView } from "framer-motion"
+import { Menu } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+
+import { Button } from "@/components/ui/button"
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
+import ScrollTo from "@/components/ui/scrollTo"
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
+import useEvent from "@/components/hooks/useEvents"
+
+const navItems = [
+  { label: "Home", href: "#", scrollTo: "home" },
+  { label: "Horoscope", href: "#", scrollTo: "horoscope" },
+  { label: "Astrology", href: "#", scrollTo: "astrology" },
+  { label: "2023 Report", href: "#", scrollTo: "report" },
+]
+
+const Header = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
+  ({ children, ...props }, ref) => {
+    const logoRef = useRef(null)
+
+    const isInView = useInView(logoRef as RefObject<Element>, {
+      once: true,
+    })
+
+    return (
+      <motion.header
+        ref={ref}
+        initial={{ opacity: 1 }}
+        animate={{ opacity: isInView ? 1 : 1 }}
+        transition={{ duration: 0.5, delay: 0.3 }}
+        className={cn(
+          "container mx-auto w-full",
+          "flex items-center justify-between",
+          "absolute left-0 right-0 top-5 z-30"
+        )}
+      >
+        <Image
+          src="/assets/images/logo.png"
+          alt="Logo"
+          width={119}
+          height={38}
+        />
+        <NavigationMenu className={cn("hidden lg:block")}>
+          <NavigationMenuList className={cn("gap-10")}>
+            {navItems.map(({ label, href, scrollTo }) => (
+              <NavigationMenuItem key={scrollTo}>
+                <Link href={href} legacyBehavior passHref>
+                  <NavigationMenuLink asChild>
+                    <ScrollTo
+                      elementId={scrollTo}
+                      className={cn(navigationMenuTriggerStyle(), "")}
+                    >
+                      {label}
+                    </ScrollTo>
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            ))}
+            {children}
+          </NavigationMenuList>
+        </NavigationMenu>
+        <Button
+          asChild
+          variant="default"
+          size="default"
+          className={cn("relative  overflow-hidden !rounded-[10px] text-lg")}
+        >
+          <Link href="#">Unlock your source now!</Link>
+        </Button>
+      </motion.header>
+    )
+  }
+)
+Header.displayName = "Header"
+
+export default Header
---
+++ src/components/ui/scrollTo.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import React from "react"
+
+import { Button, type ButtonProps } from "./button"
+
+interface ScrollToProps extends ButtonProps {
+  elementId: string
+}
+
+const ScrollTo = React.forwardRef<HTMLButtonElement, ScrollToProps>(
+  ({ elementId, children, className, ...props }, ref) => {
+    const handleScrollTo = () => {
+      const element = document.getElementById(elementId)
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" })
+        return
+      } else {
+        console.error(`Element with id ${elementId} not found`)
+      }
+    }
+
+    return (
+      <Button
+        ref={ref}
+        {...props}
+        onClick={handleScrollTo}
+        className={className}
+      >
+        {children}
+      </Button>
+    )
+  }
+)
+ScrollTo.displayName = "ScrollTo"
+
+export default ScrollTo
